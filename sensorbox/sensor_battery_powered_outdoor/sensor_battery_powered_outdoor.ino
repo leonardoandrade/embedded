921046f754de5366c29a9039fb2d1fab86ff5fb7
leonardo@andrade.io <leonardo@andrade.io>
2019-10-06 23:31:56 +0100

    set to sleep mode

--- sensorbox/sensor_battery_powered_outdoor/sensor_battery_powered_outdoor.ino
+++ sensorbox/sensor_battery_powered_outdoor/sensor_battery_powered_outdoor.ino
@@ -23,7 +23,7 @@ const char* db = INFLUXDB_DB;
 const char* http_credentials = INFLUXDB_DB_HTTP_CREDENTIALS;
 
 
-void record_value(char* sensor, float value)
+void record_temperature_and_battery(float temparature, float battery)
 {
   int tries = 5;
   Serial.println(WiFi.status());
@@ -41,12 +41,24 @@ void record_value(char* sensor, float value)
 
 
   if (client.connect("andrade.io", 443)) {
+    char data_temperature[100];
+    sprintf(data_temperature, "outdoor,sensor=temperature value=%d.%0.2d",  (int)temparature, (int)(temparature*100)%100);
 
-    char data[200];
-    sprintf(data, "outdoor,sensor=%s value=%d.%0.2d", sensor,  (int)value, (int)(value*100)%100);
+    
+    char data_battery[100];
+    sprintf(data_battery, "outdoor,sensor=battery value=%d", (int)battery);
+
     
     char authorization_header[200];
     sprintf(authorization_header, "Authorization: Basic %s", http_credentials);
+
+
+
+    char data[200];
+  
+    sprintf(data, "%s\n%s", data_temperature, data_battery);
+
+
 
     client.println("POST /influxdb/write?db=house_sensors HTTP/1.1");
     client.println("Host: " + (String)"andrade.io");
@@ -91,22 +103,31 @@ void setup(){
 
 
   // Deep sleep approach:
-  /*
+  
   // get temperature and save into remote influxdb
   sensors.requestTemperatures(); 
   float temperature_celsius = sensors.getTempCByIndex(0);
   Serial.print("Temperature = ");
-  Serial.println( temperature_celsius);
-  record_value((char*)"temperature", temperature_celsius);
-  
-  // run every 5 minutes
-  ESP.deepSleep(30e6); */
-}
+  Serial.println(temperature_celsius);
 
+  // battery level (TODO: calibrate)
+  float battery_level = analogRead(0);
+  Serial.print("Battery = ");
+  Serial.println(battery_level);
+
+
+  record_temperature_and_battery(temperature_celsius, battery_level);
+
+  // run every 15 minutes
+  Serial.println("I'm awake, but I'm going into deep sleep mode for 900 seconds");
+  ESP.deepSleep(900e6); 
+
+  
+}
 void loop()
 {
 
-
+/*
   // temperature
   sensors.requestTemperatures(); 
   float temperature_celsius = sensors.getTempCByIndex(0);
@@ -121,5 +142,6 @@ void loop()
   Serial.println(battery_level);
  
   // measure every 30 minutes
-  delay(1800000);
+  delay(30000);
+*/
 }
