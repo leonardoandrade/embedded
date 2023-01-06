dbaa64bd38db21745eb2737abacb585613156bf5
leonardo@andrade.io <leonardo@andrade.io>
2023-01-06 00:33:49 +0000

    🎪 update docs, refine blink led

--- esp32/README.md
+++ esp32/README.md
@@ -1,8 +1,10 @@
 # Sensors for ESP32 microcontroler
 
-To build: 
+To build & deploy:
 1. go to project root
-2.
+2. configure environment: `source ${ESP_ROOT}>/esp-idf/export.sh`
+3. set the target:  `idf.py set-target esp32`
+
 
 
 To monitor the serial uart: 
---
+++ esp32/blink_led/main/blink_led.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+#include "sdkconfig.h"
+
+
+
+static LED_GPIO = 33;
+
+int app_main()
+{
+
+    gpio_reset_pin(LED_GPIO);
+    gpio_set_direction(LED_GPIO, GPIO_MODE_OUTPUT);
+
+    bool state = true;
+
+    while (1) {
+        gpio_set_level(LED_GPIO, state);
+        state = !state;
+        vTaskDelay(100);
+    }
+}
