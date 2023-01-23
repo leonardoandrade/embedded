64e8cb5678e161cef14bc43955a70837fa491d00
leonardo@andrade.io <leonardo@andrade.io>
2023-01-23 01:29:41 +0000

    🎺 decouple networking code

--- esp32/dht22/main/main.c
+++ esp32/dht22/main/main.c
@@ -4,7 +4,7 @@
 #include "freertos/event_groups.h"
 #include "esp_system.h"
 #include "rom/ets_sys.h"
-#include "nvs_flash.h"
+
 #include "driver/gpio.h"
 #include "sdkconfig.h"
 #include "esp_wifi.h"
@@ -12,91 +12,14 @@
 #include "esp_log.h"
 
 #include "./WiFiCredentials.h"
-#include "../vendor/dht22.h"
+#include "../sensor/dht22.h"
+#include "../network/wifi.h"
 
 #define DHT22_PIN 33
 
 static int ERROR_LED = 14;
 
-static const char *TAG = "wifi station";
 
-static int s_retry_num = 0;
-
-static EventGroupHandle_t s_wifi_event_group;
-#define EXAMPLE_ESP_MAXIMUM_RETRY 5
-
-#define WIFI_CONNECTED_BIT BIT0
-#define WIFI_FAIL_BIT BIT1
-
-static void event_handler(void *arg, esp_event_base_t event_base,
-                          int32_t event_id, void *event_data)
-{
-    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START)
-    {
-        esp_wifi_connect();
-    }
-    else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED)
-    {
-        if (s_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY)
-        {
-            esp_wifi_connect();
-            s_retry_num++;
-            ESP_LOGI(TAG, "retry to connect to the AP");
-        }
-        else
-        {
-            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
-        }
-        ESP_LOGI(TAG, "connect to the AP fail");
-    }
-    else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP)
-    {
-        ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
-        ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
-        s_retry_num = 0;
-        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
-    }
-}
-/// from https://github.com/espressif/esp-idf/blob/master/examples/wifi/getting_started/station/main/station_example_main.c
-void connect_wifi()
-{
-
-    s_wifi_event_group = xEventGroupCreate();
-
-    ESP_ERROR_CHECK(esp_netif_init());
-
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-    esp_netif_create_default_wifi_sta();
-
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-
-    esp_event_handler_instance_t instance_any_id;
-    esp_event_handler_instance_t instance_got_ip;
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
-                                                        ESP_EVENT_ANY_ID,
-                                                        &event_handler,
-                                                        NULL,
-                                                        &instance_any_id));
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
-                                                        IP_EVENT_STA_GOT_IP,
-                                                        &event_handler,
-                                                        NULL,
-                                                        &instance_got_ip));
-
-    wifi_config_t wifi_config = {
-        .sta = {
-            .ssid = WIFI_SSID,
-            .password = WIFI_PW,
-            .threshold.authmode = WIFI_AUTH_WPA2_PSK,
-            .sae_pwe_h2e = WPA3_SAE_PWE_BOTH,
-        },
-    };
-
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
-    ESP_ERROR_CHECK(esp_wifi_start());
-}
 
 void enable_error_led()
 {
@@ -109,19 +32,6 @@ void disable_error_led()
 }
 
 
-void initialise_nvs()
-{
-    esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
-    {
-        ESP_ERROR_CHECK(nvs_flash_erase());
-        ret = nvs_flash_init();
-    }
-    ESP_ERROR_CHECK(ret);
-
-    ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
-}
-
 void app_main()
 {
 
@@ -131,8 +41,8 @@ void app_main()
     printf("DHT Reading started\n\n");
     setDHTgpio(DHT22_PIN);
 
-    initialise_nvs();
-    connect_wifi();
+    
+    connect_wifi(WIFI_SSID, WIFI_PW);
 
     int count = 0;
     while (1)
---
+++ esp32/dht22/network/wifi.h
@@ -0,0 +1,6 @@
+#ifndef WIFI_H_  
+#define wIFI_H_
+
+void connect_wifi(uint8_t[], uint8_t[]);
+
+#endif
